9c3180cec933a7afe0bd78cf442273396abf34e8
Nil Késede <nilkesede@gmail.com>
2019-12-26 14:39:29 -0300

    add env vars to heroku

--- credentials/client.js
+++ credentials/client.js
@@ -1,9 +1,15 @@
-module.exports = {
-  apiKey: 'AIzaSyDvoSyLrUbY_8xRI4n4iXyxLX044VBGTBw',
-  authDomain: 'hubtec-tasks.firebaseapp.com',
-  databaseURL: 'https://hubtec-tasks.firebaseio.com',
-  projectId: 'hubtec-tasks',
-  storageBucket: 'hubtec-tasks.appspot.com',
-  messagingSenderId: '922038563382',
-  appId: '1:922038563382:web:196c0dd920767f48a31e97'
-};
+try {
+  const buff = Buffer.from(process.env.CLIENT_CREDENTIALS, 'base64');
+  const text = buff.toString('ascii');
+  module.exports = JSON.parse(text);
+} catch (_) {
+  module.exports = {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
+  };
+}
--- credentials/server.js
+++ credentials/server.js
@@ -1,12 +1,18 @@
-module.exports = {
-  type: 'service_account',
-  project_id: 'hubtec-tasks',
-  private_key_id: '9f7aa9f99b18b8c92c0a733e007e99ef62dc39cd',
-  private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCUa8cynKrPBzek\nCnWqVON7lOQNVhWyYFx6E88V0ZER7nPNsUzSz8tK59v/XFeVo5qqIafQyEy5BTzH\n0/ez3jHwXo/PS6C/wG7BH/gzpaanPafg5vn9hq5mTGAzkik691LaEJ0ig/S7hqrk\nJ4TZkJ66ltDT5JUy4D6eNrG/Hc7IJS888g/IBu3ZX5IOCU6RXXtw+msD7AVCv/hl\nHeq9/mvD64qf15/QIimSrJoFZUjnvxKYQMrJeiWK3iB+/Ja4LkcfvRNqx8+7Bvja\nyBiUBn7G40VsySiBNWmZYHKqoSp9l0pefrs+nJ1ubr6gb9hboYMI0pZ8NnOkqJi5\nZYYGKwE3AgMBAAECggEAK4a2Cso2L3Oy5hkpK7QsaFwgSybxQvgOdsCjbQch76vJ\n60sxN+u3dzaq5mcaymgJ3hXzpH9xPJtkGUg47tddNtxMeWL49/MDqABrph3Q01/6\nATNLogWi1J5WYhldnQzWD9xqcKpZFbunFzTduX43honfIRz6gkqQIo7wrpcbwsbu\nV65w63TlqAELOhL35PwuKPS5G84sRYSigR/3hw0/n8wUFZksNCn1bg50EZlA0KvJ\nkODjHApYjYQNI1zXJbpWX3SQpna0oJmka4ePzV0Em82aggZ1/7GbJbn8zo+E++9C\nuIEyvujDw8fTxXA3oF6gitDaWgbI6YEpiInQ0CKqnQKBgQDPdoOfde8Qjp8Nrvd3\nKjdA4kd3zkH5rbwnmWPFtAuGcUfDmN6RkvTQViAgsAVso1yZd412Acrq0dL159Te\nr14KayxpHXYZtbSsDj9Q1pEC5tfVzqYZ9Qle524uUgJjJQoFnvoWyxhVm5MPqb5R\nz7DFngpwwQrW7H9Hkhc4zPw5FQKBgQC3JRdrSzdKfuji5oQlT6ONiAVMgwq53M9V\n1KAJLG/QMLmOkaKh+JrIaPp+by1i260iWNSvIskwPfJ+/GLOLzHUewmlyvrq/wmg\nenU462Gf2rbDt54+pmQDguRwyXzn5LnoZfvFeexkewoYeseK11O9k+URsW6IHc98\n3Bxk8lQMGwKBgHP+TUVAyPM26pLZ+p5zygTK/y5q8dRJYOO7DV+idLcsavVfaUF3\nKs24+amNANtm0wUzJLKP+gYkXpECl7AR2MB2f5Qd1sQ1Jdj09k2aSeWXb6GpMpFc\nwWqAdsgLLi6ErP/LjQRldSuYGjctrq1LNAipfijgpdDY1akMc1NcuMCpAoGAIWoG\nBNBbTR8sdEFkeL4389mORfBeo5xyw5Eghavy1oQdRrKxfmWyL8Bfe3PjTvcPdoXI\ngA+Tr1aOCLcZe87Gg6LTFsma0AcqBXpyKqlfTWAFGl5E499NvgpCjYVlYZWsiYcZ\n0WS2SXrdU6wK/fzdTWhPUdQSe4kc0wYBtDU2OmMCgYALCPvQ1gg5TiKf1ZY9/GiK\ncL0XUZoL5x80raRp2MC5ogap2a7CpCQtKPez5OUUQimfX3wsZwt5aNF/m4/tnrGH\n2YALbs2uZxJJczu2zvCWEILmoO6Eg69qZzIgLrT+W2apkPGehmIxZs/DVt7OtqO9\nuGVhXK3Hmi0vLxqBcqOKQA==\n-----END PRIVATE KEY-----\n',
-  client_email: 'firebase-adminsdk-uz3ij@hubtec-tasks.iam.gserviceaccount.com',
-  client_id: '108658246406293519555',
-  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-  token_uri: 'https://oauth2.googleapis.com/token',
-  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-uz3ij%40hubtec-tasks.iam.gserviceaccount.com'
-};
+try {
+  const buff = Buffer.from(process.env.SERVER_CREDENTIALS, 'base64');
+  const text = buff.toString('ascii');
+  module.exports = JSON.parse(text);
+} catch (_) {
+  module.exports = {
+    type: '',
+    project_id: '',
+    private_key_id: '',
+    private_key: '',
+    client_email: '',
+    client_id: '',
+    auth_uri: '',
+    token_uri: '',
+    auth_provider_x509_cert_url: '',
+    client_x509_cert_url: ''
+  };
+}
