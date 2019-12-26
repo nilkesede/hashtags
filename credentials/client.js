6f87ae9c9a6a25a51e346711b347bdbb9ff020fc
Nil Késede <nilkesede@gmail.com>
2019-12-26 11:38:28 -0300

    add credentials

---
+++ credentials/client.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: 'AIzaSyDvoSyLrUbY_8xRI4n4iXyxLX044VBGTBw',
+  authDomain: 'hubtec-tasks.firebaseapp.com',
+  databaseURL: 'https://hubtec-tasks.firebaseio.com',
+  projectId: 'hubtec-tasks',
+  storageBucket: 'hubtec-tasks.appspot.com',
+  messagingSenderId: '922038563382',
+  appId: '1:922038563382:web:196c0dd920767f48a31e97'
+};
