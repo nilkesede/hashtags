416977f96fb3cb1e694e6509f404aec40914656c
Nil Késede <nil@ksde.pw>
2020-02-12 12:52:54 -0300

    clean theme file

--- config/theme.js
+++ config/theme.js
@@ -1,16 +1,6 @@
 export default {
   colors: {
-    pink: '#ffd8c7',
-    darkGray: '#102622',
-    gray: '#787878',
-    lightGray: '#D8D8D8',
-    blue: '#bef2ff',
-    yellow: '#FCFFA6',
-    white: '#ffffff',
-    black: '#000000',
-    facebookBlue: '#4267B2',
-    lightGreen: '#e7ffa6',
-    darkGreen: '#85955c'
+    blue: '#0d6efd'
   },
-  primaryColor: 'lightGreen'
+  primaryColor: 'blue'
 }
