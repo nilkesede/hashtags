37267cf5b011443f2382ff0e6649fa5349c1116e
Nil Késede <nil@ksde.pw>
2020-02-13 14:40:56 -0300

    update head component

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -1,32 +1,27 @@
-import React, {Component} from 'react'
 import NextHead from 'next/head'
 import {string} from 'prop-types'
 
-export default class Head extends Component {
-  static propTypes = {
-    title: string.isRequired,
-    description: string
-  }
+const Head = ({title, description}) => (
+  <NextHead>
+    <meta charSet="UTF-8"/>
+    <title>{title}</title>
 
-  static defaultProps = {
-    description: ''
-  };
+    <meta name="description" content={description}/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="theme-color" content="#222"/>
 
-  render() {
-    const {title, description} = this.props
+    <link rel="manifest" href="/manifest.json"/>
+    <link rel="icon" href="/favicon.ico"/>
+  </NextHead>
+)
 
-    return (
-      <NextHead>
-        <meta charSet="UTF-8"/>
-        <title>{title}</title>
-
-        <meta name="description" content={description}/>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <meta name="theme-color" content="#222"/>
-
-        <link rel="manifest" href="/manifest.json"/>
-        <link rel="icon" href="/favicon.ico"/>
-      </NextHead>
-    )
-  }
+Head.propTypes = {
+  title: string.isRequired,
+  description: string
 }
+
+Head.defaultProps = {
+  description: ''
+}
+
+export default Head
